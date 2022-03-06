651fe7ad64bc496a4f0e2707249c6f6da0e34fdf
Hanley Lee <hanley.lei@gmail.com>
2022-03-06 12:48:35 +0800

    daily 2022-03-06

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'My app',
-    home: TutorialHome(),
+    home: Home(),
   ));
 }
 
-class TutorialHome extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,87 +28,28 @@ class TutorialHome extends StatelessWidget {
       ),
       body: Center(
         // child: Text('Hello, world'),
-        child: Counter(),
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Album'),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: const FloatingActionButton(
         tooltip: 'Add',
         child: Icon(Icons.add),
         onPressed: null,
       ),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('Mybuton was tapped!');
-      },
-      child: Container(
-        height: 50,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightGreen,
-        ),
-        child: Center(
-          child: Text('Engage1'),
-        ),
-      ),
-    );
-  }
-}
-
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({required this.count});
-
-  final int count;
-  @override
-  Widget build(BuildContext context) {
-    return Text('count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({required this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: onPressed,
-      child: Text('Increment'),
-    );
-  }
-}
-
-class Counter extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _CounterState();
-  }
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CounterIncrementor(onPressed: _increment),
-        CounterDisplay(count: _counter),
-      ],
     );
   }
 }
