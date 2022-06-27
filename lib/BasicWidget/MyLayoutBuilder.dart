b3c7f9e7f0fc5270e7a021423ead49af07b84c17
Hanley Lee <hanley.lei@gmail.com>
2022-06-27 22:07:26 +0800

    daily 2022-06-27

---
+++ lib/BasicWidget/MyLayoutBuilder.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyLayoutBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+    LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+      print("hl_constraints: $constraints");
+      return Container(
+        height: 56,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: const Text('bottom'),
+      );
+    })
+;
+  }
+}
